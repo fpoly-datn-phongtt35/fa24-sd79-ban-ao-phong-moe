85cc9ca7763caa087fd5adf60a2dfba17d499211
Nong Hoang Vu <vunhph33506@fpt.edu.vn>
2024-09-26 14:30:03 +0700

    upload project

---
+++ BE-MOE/src/main/java/sd79/BeMoeApplication.java
@@ -0,0 +1,13 @@
+package sd79;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMoeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeMoeApplication.class, args);
+    }
+
+}
